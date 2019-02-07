26b1c45a2015b0ab6535265fcdafdef6cf16ba59
francis-mwas <mwangif184@gmail.com>
2019-02-07 11:11:56 +0300

    [finishes #163768106] added input validations

--- app/admin/admin.py
+++ app/admin/admin.py
@@ -4,135 +4,107 @@ from flask_restful import Resource, reqparse
 """ local imports """
 from ..models.models import parties,Parties, CreatePoliticalOffice, offices
 
-""" create class Parties """
+from validations import validations
+
+
 class Party(Resource):
+    """ create class Parties """
     parser = reqparse.RequestParser(bundle_errors=True)
 
     parser.add_argument('name', type=str, required=True, help='Please fill in this field')
     parser.add_argument('hqAddress', type=str, required=True, help='Please fill in this field')
     parser.add_argument('logoUrl', type=str, required=True, help='This field cant be empty')
 
-    """create post party method"""
+    
     def post(self):
+         """create post party method"""
          party_data = Party.parser.parse_args()
 
          name = party_data['name']
          hqAddress = party_data['hqAddress']
          logoUrl = party_data['logoUrl']
 
-         """ check if party name is valid """
-         if name.isdigit():
-             return{
-                 "status":400,
-                 "Message": "Invalid party name, name must be characters only"
-                 }, 400
+         validate_data = validations.Validations()
 
-         if hqAddress.isdigit():
-             return {
-                 "status":400,
-                 "Message": "Invalid party hqAddress, it must be characters only"
-                 }, 400
+         """check to see if the input strings are valid"""
+         if not validate_data.validate_input_fields(name):
+            return {"status":400,"Message": "Please enter valid name"}, 400
+         if not validate_data.validate_input_fields(hqAddress):
+            return {"status":400,"Message": "Please enter valid headquarter name"}, 400
+         if not validate_data.validate_url(logoUrl):
+            return {"status":400, "Message": "Please enter a valid logo url"}, 400
+         
+         """ check of party exist """
+         party_name = Parties().get_party_by_name(name)
+         if party_name:
+             return {"status": 400, "Message": "This party already exist"}, 400
 
-         """ check to see if the logo url is a string only"""
-         if logoUrl.isdigit():
-            return{
-                "status":400,
-                "Message": "Invalid logo url, it must be characters only"
-                }, 400
-
-
-         """ check if party already exists before creation """
-         if Parties().get_party_by_name(name):
-            return {
-                "status": 400,
-                "Message": "This party already exists"
-                }, 400
-
+         
          party = Parties(name,hqAddress,logoUrl)
          parties.append(party)
          if party:
-                 return {
-                     "status":201,
-                     "Message": "Party reqistered successfully"
+                 return {"status":201,"Message": "Party reqistered successfully"
                      }, 201
-         return {
-             "status": "400",
-             "Message": "Party not created"}, 400
+         return {"status": "400","Message": "Party not created"}, 400
              
 
-    """fetch all political parties """
+  
     def get(self):
-        return{
-            "status":200,
-            "parties":[party.serialize() for party in parties]
-        }
-"""get a specific political party by using id """
+        """fetch all political parties """
+        return{"status":200,"parties":[party.serialize() for party in parties]}
+
+
 class GetSpecificParty(Resource):
+    """get a specific political party by using id """
     parser = reqparse.RequestParser()
-    parser.add_argument('name', type=str, required=True, help='Please fill in this field')
-    parser.add_argument('hqAddress', type=str, required=True, help='Please fill in this field')
-    parser.add_argument('logoUrl', type=str, required=True, help='This field cant be empty')
+    parser.add_argument('name', type=str, required=True, 
+    help='Please fill in this field')
+    parser.add_argument('hqAddress', type=str, required=True,
+     help='Please fill in this field')
+    parser.add_argument('logoUrl', type=str, required=True,
+     help='This field cant be empty')
     
-    """ get a specific party by id """
+    
     def get(self, id):
+        """ get a specific party by id """
         party = Parties().get_specific_party_by_id(id)
         if not party:
-            return {
-                "status": 404,
-                "Message": "That party does not exist"
-                }, 404
+            return {"status": 404,"Message": "That party does not exist"}, 404
 
-        return {
-            "party": party.serialize(),
-            "status": 200
-            }, 200
+        return {"party": party.serialize(),"status": 200}, 200
 
-    """ delete a specific party """
+   
     def delete(self,id):
+         """ delete a specific party """
 
          party = Parties().get_specific_party_by_id(id)
 
          if not party:
-            return {
-                "status": 404,
-                "message": "this party does not exist"
-            }
+            return {"status": 404,"message": "this party does not exist"},404
          else:
             parties.remove(party)
-            return {
-                "status": 200,
-                "Message": "party deleted successfully"
-            }
+            return {"status": 200,"Message": "party deleted successfully"}
 
-    """ update specif party details """
+   
     def patch(self, id):
+        """ update specif party details """
         update_party =GetSpecificParty.parser.parse_args()
         name = update_party['name']
         hqAddress = update_party['hqAddress']
         logoUrl = update_party['logoUrl']
 
+        validate_data = validations.Validations()
 
-
-        """ check if party name is valid """
-        if name.isdigit():
-             return{
-                 "status":400,
-                 "Message": "Invalid party name, name must be characters only"
-                 }, 400
-
-        if hqAddress.isdigit():
-             return {
-                 "status":400,
-                 "Message": "Invalid party hqAddress, it must be characters only"
-                 }, 400
-
-        """ check to see if the logo url is a string only"""
-        if logoUrl.isdigit():
-            return{
-                "status":400,
-                "Message": "Invalid logo url, it must be characters only"
-                }, 400
-
+        """check to see if the input strings are valid"""
+        if not validate_data.validate_input_fields(name):
+            return {"status":400,"Message": "Please enter valid name"}, 400
+        if not validate_data.validate_input_fields(hqAddress):
+            return {"status":400,"Message": "Please enter valid "
+            "headquarter name"}, 400
+        if not validate_data.validate_url(logoUrl):
+            return {"status":400, "Message": "Please enter a "
+            "valid logo url"}, 400
+     
         party = Parties().get_specific_party_by_id(id)
         if not party:
             return {
@@ -153,51 +125,44 @@ class GetSpecificParty(Resource):
 
 
 
-""" create office """
-class CreateOffice(Resource):
 
+class CreateOffice(Resource):
+    """ create office """
     parser = reqparse.RequestParser(bundle_errors=True)
 
-    parser.add_argument('name', type=str, required=True, help='Please fill in this field')
-    parser.add_argument('Type', type=str, required=True, help='Please fill in this field')
+    parser.add_argument('name', type=str, 
+        required=True, help='Please fill in this field')
+    parser.add_argument('Type', type=str, 
+        required=True, help='Please fill in this field')
     
-    """ create post office method """
+    
     def post(self):
+        """ create post office method """
 
         office_data = CreateOffice.parser.parse_args()
 
         name = office_data['name']
         Type = office_data['Type']
 
-        """check if office name is valid """
-        if name.isdigit():
-             return{
-                 "status":400,
-                 "Message": "Invalid office name, name must be characters only"
-                 }, 400
-        if Type.isdigit():
-             return{
-                 "status":400,
-                 "Message": "Invalid office type, type must be characters only"
-                }, 400
+        """ validate office data before submiiting """
+        validate_office_data = validations.Validations()
+        if not validate_office_data.validate_input_fields(name):
+            return {"status":400,"Message": "Please enter " 
+            "valid office name"}, 400
+        if not validate_office_data.validate_input_fields(Type):
+            return {"status":400,"Message": "Please enter valid "
+            " office type"}, 400
 
-        """ check if oofice already exists before creation """
-        if CreatePoliticalOffice().get_office_by_name(name):
-            return {
-                "status": 400,
-                "Message": "This office already exists"
-                }, 400
-
+        office_exist = CreatePoliticalOffice().get_office_by_name(name)
+        if office_exist:
+            return {"Status": 400, "Message": "Office name "
+            "already exist"},400
 
         office = CreatePoliticalOffice(name, Type)
         offices.append(office)
         if office:
-            return {
-                     "status":201,
-                     "Message": "New office created successfully"
-                }, 201
-
-
+            return {"status":201,"Message": "New office "
+            "created successfully"}, 201
 
     """ fetch all offices """
     def get(self):
@@ -208,24 +173,19 @@ class CreateOffice(Resource):
         }
         
         
-                
-
-
-
-
-
-""" get a specific political office by id """
 class GetSpecificOffice(Resource):
+    """ get a specific political office by id """
     def get(self, office_id):
+  
         office = CreatePoliticalOffice().get_office_by_id(office_id)
+        
         if not office:
-            return {
-                "status": 404,
-                "Message": "That office does not exist"
-            }
-        return {
-            "status": 200,
-            "Office": office.serializer()
-        }, 200
+            return {"Status":400, "Message": "This office does not exist"}
+        else:
+             return {"status": 200,"Office": office.serializer()}, 200
+        
+            
+
+        
 
 
--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,24 +1,33 @@
 from flask_restful import Resource, reqparse
-
+"""local imports"""
 from ..models.models import User, users
+from validations import validations
 
-
-"""creating classs user signup"""
 
 class UserSignUp(Resource):
+    """creating classs user signup"""
+    parser = reqparse.RequestParser(bundle_errors=True)
 
-    parser = reqparse.RequestParser()
+    parser.add_argument('firstname', type=str, required=True,
+                        help='This field is required')
+    parser.add_argument('lastname', type=str, required=True,
+                        help='This field is required')
+    parser.add_argument('othername', type=str, required=True,
+                        help='This field is required')
+    parser.add_argument('email', type=str, required=True,
+                        help='This field is required')
+    parser.add_argument('phoneNumber', type=str,
+                        required=True, help='This field is required')
+    parser.add_argument('passportUrl', type=str,
+                        required=True, help='This field is required')
+    parser.add_argument('isAdmin', type=str, required=True,
+                        help='This field is required')
+    parser.add_argument('password', type=str, required=True,
+                        help='This is field is required')
 
-    parser.add_argument('firstname', type=str, required=True, help='This field is required')
-    parser.add_argument('lastname', type=str, required=True, help='This field is required')
-    parser.add_argument('othername', type=str, required=True, help='This field is required')
-    parser.add_argument('email', type=str, required=True, help='this field is required')
-    parser.add_argument('phoneNumber', type=str, required=True, help='This field is required')
-    parser.add_argument('passportUrl', type=str, required=True, help='This field is required')
-    parser.add_argument('isAdmin', type=str, required=True, help='this field is required')
-
-    """ create user account """
+   
     def post(self):
+        """ create user account """
         user_data = UserSignUp().parser.parse_args()
 
         firstname = user_data['firstname']
@@ -26,17 +35,53 @@ class UserSignUp(Resource):
         othername = user_data['othername']
         email = user_data['email']
         phoneNumber = user_data['phoneNumber']
-        passportUrl  = user_data['passportUrl']
+        passportUrl = user_data['passportUrl']
         isAdmin = user_data['isAdmin']
+        password = user_data['password']
 
+        """validate user data before submitting """
+        validate_user_data = validations.Validations()
 
-        user = User(firstname, lastname, othername, email,phoneNumber,passportUrl,bool(isAdmin))
+        if not validate_user_data.validate_input_fields(firstname):
+            return {"status": 400, "Message": "Please enter a valid "
+            "firstname, with atleast characters"}, 400
+        if not validate_user_data.validate_input_fields(lastname):
+            return {"status": 400, "Message": "Firstname should be "
+            " atleast 3 characters and characters only"}, 400
+        if not validate_user_data.validate_input_fields(othername):
+            return {"status": 400, "Message": "Othername should "
+            "be atleast 3 characters and characters only"}, 400
+        if not validate_user_data.validate_email(email):
+            return {"status": 400, "Message": "Enter valid "
+            "email address with only one @ and one dot"}, 400
+        if not validate_user_data.validate_phone_number(phoneNumber):
+            return {"status": 400, "Message": "Phone number " 
+            "should be atleast 10 characters"}, 400
+        if isAdmin not in range(0, 2):
+            return {"message": "Is admin field should have a range of"
+            "between 0 and 1"}, 400
+
+        if not validate_user_data.validate_url(passportUrl):
+            return {"status": 400, "Message": "Enter valid "
+            "passport url ending with an image extension"}, 400
+        if not validate_user_data.validate_password(password):
+            return {"status": 400, "Message": "Password must "
+            "be between 3 and 10 alphanumeric characters"}, 400
+
+       
+        """ check if user already exists """
+        user_exist = User().get_user_by_email(email)
+        if user_exist:
+            return {"status": 400, "Message": "This user already exist"}, 400
+
+        user = User(firstname, lastname, othername, email,
+                    phoneNumber, passportUrl, bool(isAdmin), password)
 
         users.append(user)
         return {
             "status": 201,
             "Message": "Your account created successfully"
         }, 201
-
-
-
+    """ get all users """
+    def get(self):
+        return {"status": 200, "users":[user.serialize() for user in users ]}, 200
