721829d175c88e70c9612872578a99d908a98c25
francis-mwas <mwangif184@gmail.com>
2019-02-21 12:06:57 +0300

    [Ch #164118543] added db port

--- instance/config.py
+++ instance/config.py
@@ -9,6 +9,7 @@ class Config:
 
     SECRET_KEY = os.getenv('SECRET_KEY')
     DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = os.getenv('DB_PORT')
     DB_USERNAME = os.getenv('DB_USERNAME')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_NAME = os.getenv('DB_NAME')
