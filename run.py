bb7ed20e220aea38f59d547f689174429a0ea424
francis-mwas <mwangif184@gmail.com>
2019-02-21 13:26:24 +0300

    [Ch #164118543] added migrations on procfile

--- run.py
+++ run.py
@@ -4,7 +4,6 @@ from app.models.models import Parties
 app = create_app(os.getenv("APP_SETTINGS") or "default")
 
 
-
 if __name__ == '__main__':
      """run flask application in debug mode."""
      app.run(debug=True)
